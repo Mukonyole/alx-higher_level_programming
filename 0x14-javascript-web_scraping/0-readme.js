e4b725df6c1694a9aa9717ac310e53f091c82516
Evans Mukonyole <emukonyole@gmail.com>
2024-07-30 06:12:48 -0700

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 " a script that reads and prints the content of a file."
-
-
 const filePath = process.argv[2];
 const fs = require('fs');
 fs.readFile(filePath, 'utf8', function (err, data) {
